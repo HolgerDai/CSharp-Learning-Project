9241e6f9772cdbef4d4eff70a2e75dd9d0644d66
Holger-T043 <956321989@qq.com>
2024-06-25 13:06:09 +0800

    测试Git

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -5,8 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            //测试和X1 同步，发送消息
-            //X1收到消息，更新并回传
+            Console.WriteLine("Hello, World!");
         }
     }
 }

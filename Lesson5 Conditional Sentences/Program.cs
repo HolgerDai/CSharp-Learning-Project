3f2beda33099e125fe8994b7f6bfd08ad3b90de2
Holger-T043 <956321989@qq.com>
2024-06-26 18:01:43 +0800

    Created Lesson5 Conditional Sentences

---
+++ Lesson5 Conditional Sentences/Program.cs
@@ -0,0 +1,38 @@
+﻿//本程序中包含以下内容
+//1.C#中的If条件语句写法为，if(条件){}，不需要endif
+//2.
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //成绩判断程序
+        Console.WriteLine("请输入成绩，程序会自动进行等级判断:");
+        int score = Convert.ToInt32(Console.ReadLine());
+
+        //C#中，if条件语句的写法为，if(条件){}
+        if (score >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if(score >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if(score >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("E");
+        }
+    
+    }
+    
+}
